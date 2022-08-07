b637d1569d5b4d2c191a84cf93d36c02c6e08dcd
Anon <here@now.com>
2022-08-07 16:06:02 -0400

    Issue #1: Solves by using '0.0.0.0'

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 from http.server import HTTPServer
 from server import Server
 
-HOST_NAME = 'localhost'
+# When containerized, '0.0.0.0' allows you to hit localhost:8000 from your host machine:
+HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 8000
 
 if __name__ == '__main__':
